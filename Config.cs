aa9d6a3a90991a16cd926f104f41861ef1101efa
DeathWrench <45341450+DeathWrench@users.noreply.github.com>
2024-03-23 23:24:33 -0400

    1.2.0

--- Config.cs
+++ Config.cs
@@ -125,7 +125,7 @@ namespace BestestTVModPlugin
                 case Key.Pause:
                 case Key.NumpadEnter:
                 case Key.NumpadDivide:
-                    return "[÷]";
+                    return "[Ã·]";
                 case Key.NumpadMultiply:
                     return "[*]";
                 case Key.NumpadPlus:
@@ -212,7 +212,7 @@ namespace BestestTVModPlugin
             tvLightEnabled = cfg.Bind("Options", "Television Lights", true, "Do lights cast from television? If using Scaleable Television set this to false.");
             tvOnAlways = cfg.Bind("Options", "TV Always On", false, "Should the TV stay on after it's been turned on?\n Warning: TV Skips After Off On will skip twice as much with this enabled");
             tvPlaysSequentially = cfg.Bind("Options", "TV Plays Sequentially", true, "Play videos in order or loop?\n");
-            tvSkipsAfterOffOn = cfg.Bind("Options", "TV Skips After Off On", false, "Should what is currently playing be skipped after the television is turned off and back on again?\n Warning: Minor UI bug where current channel will be + 1 of what it actually is if Enable Channels is checked");
+            tvSkipsAfterOffOn = cfg.Bind("Options", "TV Skips After Off On", false, "Should what is currently playing be skipped after the television is turned off and back on again?\n");
             enableSeeking = cfg.Bind("Options", "Enable Seeking", true, "Use brackets to fast forward or rewind?");
             enableChannels = cfg.Bind("Options", "Enable Channels", true, "Use comma or period to skip videos?");
             mouseWheelVolume = cfg.Bind("Options", "Mouse Wheel Volume", true, "Should the mouse wheel control the volume?");
